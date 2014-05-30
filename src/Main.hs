ada6692959f01ea5a5f2aa30714157abc101db75
Michael Neale <michael.neale@gmail.com>
2014-05-30 21:06:44 +1000

    more

--- src/Main.hs
+++ src/Main.hs
@@ -1,42 +1,59 @@
-{-# LANGUAGE OverloadedStrings, ScopedTypeVariables, RecordWildCards,
-             QuasiQuotes, TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
 
-module Main where
-
-import Prelude hiding (product)
-
-import Network.HTTP.Types
-import Network.Wai.Middleware.RequestLogger
-import Network.Wai.Middleware.Static
-import Web.Scotty
-import Data.Aeson.Types           (ToJSON)
+import Data.Aeson
+import Data.Text
 import Control.Applicative
-import Control.Monad.IO.Class (liftIO)
+import Control.Monad
+import qualified Data.ByteString.Lazy as B
+import Network.HTTP.Conduit (simpleHttp)
+import GHC.Generics
 
+-- | Type of each JSON entry in record syntax.
+data Person =
+  Person { firstName  :: !Text
+         , lastName   :: !Text
+         , age        :: Int
+         , likesPizza :: Bool
+           } deriving (Show,Generic)
 
+-- Instances to convert our type to/from JSON.
 
-import Models
+instance FromJSON Person
+instance ToJSON Person
 
-main :: IO()
+-- | Location of the local copy, in case you have it,
+--   of the JSON file.
+jsonFile :: FilePath
+jsonFile = "pizza.json"
+
+-- | URL that points to the remote JSON file, in case
+--   you have it.
+jsonURL :: String
+jsonURL = "http://daniel-diaz.github.io/misc/pizza.json"
+
+-- Move the right brace (}) from one comment to another
+-- to switch from local to remote.
+
+{--
+-- Read the local copy of the JSON file.
+getJSON :: IO B.ByteString
+getJSON = B.readFile jsonFile
+--}
+
+{--}
+-- Read the remote copy of the JSON file.
+getJSON :: IO B.ByteString
+getJSON = simpleHttp jsonURL
+--}
+
+main :: IO ()
 main = do
-  
-    scotty 3000 $ do
-      middleware logStdoutDev
-      middleware $ staticPolicy (noDots >-> addBase "public")
-      get "/" $
-        redirect "index.html"
-      
-      get "/text" $ 
-        liftIO example1 >>= text
-
-
-      get "/jenkins" $
-        liftIO jenkins >>= text
-
-      get "/jobs" $ 
-        liftIO jenkinsJobAndBack >>= json
-
-      notFound $ do
-        status notFound404
-        html $ "<h1>Not found :(</h1>"
-
+ -- Get JSON data and decode it
+ d <- (eitherDecode <$> getJSON) :: IO (Either String [Person])
+ -- If d is Left, the JSON was malformed.
+ -- In that case, we report the error.
+ -- Otherwise, we perform the operation of
+ -- our choice. In this case, just print it.
+ case d of
+  Left err -> putStrLn err
+  Right ps -> print ps
